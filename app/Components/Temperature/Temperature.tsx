49de7afcee96ddc3224799a7360c1560f98bb11b
maclinz <tafadzwamucheke@gmail.com>
2024-02-11 13:40:01 +0000

    feat: temp component complete

--- app/Components/Temperature/Temperature.tsx
+++ app/Components/Temperature/Temperature.tsx
@@ -1,8 +1,16 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useGlobalContext } from "@/app/context/globalContext";
-import { clearSky, cloudy, drizzleIcon, rain, snow } from "@/app/utils/Icons";
+import {
+  clearSky,
+  cloudy,
+  drizzleIcon,
+  navigation,
+  rain,
+  snow,
+} from "@/app/utils/Icons";
 import { kelvinToCelsius } from "@/app/utils/misc";
+import moment from "moment";
 
 function Temperature() {
   const { forecast } = useGlobalContext();
@@ -40,15 +48,46 @@ function Temperature() {
     }
   };
 
+  // Live time update
+  useEffect(() => {
+    // upadte time every second
+    const interval = setInterval(() => {
+      const localMoment = moment().utcOffset(timezone / 60);
+      // custom format: 24 hour format
+      const formatedTime = localMoment.format("HH:mm:ss");
+      // day of the week
+      const day = localMoment.format("dddd");
+
+      setLocalTime(formatedTime);
+      setCurrentDay(day);
+    }, 1000);
+  }, []);
+
   return (
     <div
-      className="pt-6 pb-5 border rounded-lg flex flex-col 
+      className="pt-6 pb-5 px-4 border rounded-lg flex flex-col 
         justify-between dark:bg-dark-grey shadow-sm dark:shadow-none"
     >
       <p className="flex justify-between items-center">
         <span className="font-medium">{currentDay}</span>
         <span className="font-medium">{localTime}</span>
       </p>
+      <p className="pt-2 font-bold flex gap-1">
+        <span>{name}</span>
+        <span>{navigation}</span>
+      </p>
+      <p className="py-10 text-9xl font-bold self-center">{temp}°</p>
+
+      <div>
+        <div>
+          <span>{getIcon()}</span>
+          <p className="pt-2 capitalize text-lg font-medium">{description}</p>
+        </div>
+        <p className="flex items-center gap-2">
+          <span>Low: {minTemp}°</span>
+          <span>High: {maxTemp}°</span>
+        </p>
+      </div>
     </div>
   );
 }
