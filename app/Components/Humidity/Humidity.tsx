ac6c0d20548c48e20478dd24557338a273383664
maclinz <tafadzwamucheke@gmail.com>
2024-02-12 23:48:12 +0000

    feat: Humidity component

--- app/Components/Humidity/Humidity.tsx
+++ app/Components/Humidity/Humidity.tsx
@@ -1,8 +1,40 @@
 "use client";
+import { useGlobalContext } from "@/app/context/globalContext";
+import { droplets } from "@/app/utils/Icons";
+import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 
 function Humidity() {
-  return <div>Humidity</div>;
+  const { forecast } = useGlobalContext();
+
+  if (!forecast || !forecast?.main || !forecast?.main?.humidity) {
+    return <Skeleton className="h-[12rem] w-full" />;
+  }
+
+  const { humidity } = forecast?.main;
+
+  const getHumidityText = (humidity: number) => {
+    if (humidity < 30) return "Dry: May cause skin irritation";
+    if (humidity >= 30 && humidity < 50)
+      return "Comfortable: Ideal for health and comfort";
+    if (humidity >= 50 && humidity < 70)
+      return "Moderate: Sticky, may increase allergens";
+    if (humidity >= 70) return "High: Uncomfortable, mold growth risk";
+    return "Unavailable: Humidity data not available";
+  };
+
+  return (
+    <div className="pt-6 pb-5 px-4 h-[12rem] border rounded-lg flex flex-col gap-8 dark:bg-dark-grey shadow-sm dark:shadow-none">
+      <div className="top">
+        <h2 className="flex items-center gap-2 font-medium">
+          {droplets} Humidity
+        </h2>
+        <p className="pt-4 text-2xl">{humidity}%</p>
+      </div>
+
+      <p className="text-sm">{getHumidityText(humidity)}.</p>
+    </div>
+  );
 }
 
 export default Humidity;
