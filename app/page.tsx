179de06cd74264cf636f9aba1a7d41ce33261ec8
maclinz <tafadzwamucheke@gmail.com>
2025-05-22 00:16:12 +0100

    fix: window

--- app/page.tsx
+++ app/page.tsx
@@ -23,10 +23,12 @@ export default function Home() {
   const getClickedCityCords = (lat: number, lon: number) => {
     setActiveCityCoords([lat, lon]);
 
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    if (typeof window !== "undefined") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
